75d4c259ec220b38add02e998e9cf8cdf2ebf53c
joostkouwenhoven <joostkouwenhoven@gmail.com>
2016-02-16 21:04:20 +0100

    2

--- src/test/scala/com/gatling/demo/gatling/setup/Google.scala
+++ src/test/scala/com/gatling/demo/gatling/setup/Google.scala
@@ -14,7 +14,7 @@ import scala.concurrent.duration._
  * the application.conf file. When changes have to be made to which scenarios to run, that
  * information is gathered in the Scenarios object.
  */
-class Demo extends TargetsIoSimulation {
+class Google extends TargetsIoSimulation {
 
 
   /** 
